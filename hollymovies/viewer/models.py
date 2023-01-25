e41ed848597b77ec5a353d7a9a9dad7364bae175
MartyMarkCz <martin.mika.de@gmail.com>
2023-01-25 17:21:55 +0100

    Lekce 23.1.2023

---
+++ hollymovies/viewer/models.py
@@ -0,0 +1,16 @@
+from django.db.models import Model, CharField, ForeignKey, DO_NOTHING, IntegerField, DateField, \
+    TextField, DateTimeField
+
+
+# Create your models here.
+class Genre(Model):
+    name = CharField(max_length=16)
+
+
+class Movie(Model):
+    title = CharField(max_length=128)
+    genre = ForeignKey(Genre, on_delete=DO_NOTHING)
+    rating = IntegerField()
+    released = DateField()
+    description = TextField()
+    created = DateTimeField(auto_now_add=True)
