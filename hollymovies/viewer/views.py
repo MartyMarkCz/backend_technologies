e0caef6b20b27e4ca67df78e627b4b9cdcc42f21
MartyMarkCz <martin.mika.de@gmail.com>
2023-01-26 18:04:49 +0100

    Lekce 26.1.2023

--- hollymovies/viewer/views.py
+++ hollymovies/viewer/views.py
@@ -1,25 +1,22 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 from viewer.models import *
-
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
-
+    return render(request, 'home.html')
 
 def hello(request, s):
-    adjectives = ['wonderful', 'nice', 'blue', s]
+    adjectives = ['wunderfull', 'nice', 'blue', s]
     context = {'adjectives': adjectives, 'name': 'Martin'}
-    return render(request, 'moviehello.html', context)
-    # return HttpResponse(f"Hello, {s} world!")
-
+    return render(request, 'hello.html', context)
+    #return HttpResponse(f"Hello, {s} world!")
 
 def hello2(request):
     s = request.GET.get('s', '')
     return HttpResponse(f"Hello, {s} world!")
 
-
 def movies(request):
     movies = Movie.objects.all()
-    context = {'movie': movies}
+    context = {'movies': movies}
+    return render(request, 'movies.html', context)
