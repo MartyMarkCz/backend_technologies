ecc6440e59dc3b07510a17bc77c6c5bfc8dda8a3
MartyMarkCz <martin.mika.de@gmail.com>
2023-01-27 21:00:13 +0100

    Lekce 27.1.2023

--- hollymovies/viewer/admin.py
+++ hollymovies/viewer/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 from viewer.models import *
-
 
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Movie)
+admin.site.register(AgeRestriction)
+admin.site.register(Country)
+admin.site.register(AwardCategory)
+admin.site.register(Image)
+admin.site.register(Rating)
+admin.site.register(Award)
+admin.site.register(Staff)
